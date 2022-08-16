31a3e0c88042df60d7ed2c760a619b9b5255ccb8
54u7 <99465123+54u7@users.noreply.github.com>
2022-08-16 16:48:32 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Proyecto_web
+Esta es el proyecto final de Saul duran, esta es la captura de pantalla: 
+
+![Mi captura de pantallla](img/captura.jpg)
---
+++ js/script.js
@@ -0,0 +1,11 @@
+const $btnSignIn= document.querySelector('.btn-Sign-In'),
+      $btnSignUp = document.querySelector('.btn-Sign-Up'),  
+      $signUp = document.querySelector('.sign-up'),
+      $signIn  = document.querySelector('.sign-in');
+
+document.addEventListener('click', e => {
+    if (e.target === $btnSignIn || e.target === $btnSignUp) {
+        $signIn.classList.toggle('active');
+        $signUp.classList.toggle('active')
+    }
+});
